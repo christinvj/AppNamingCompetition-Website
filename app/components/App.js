5efde326628de6077114dad6eda3ffc6f3ee3cbd
Christin V Jose <christinvj@gmail.com>
2017-04-06 01:08:24 +0530

    Basic files created

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var MyComponentClass = React.createClass ({
+	render: function () {
+		return (
+			<h1>Hello world</h1>
+		);
+	}
+});
+
+module.exports = MyComponentClass;
